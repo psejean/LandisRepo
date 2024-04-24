cd983d460d554bd56e23219fa2ace35c59f0080b
psejean <150192056+psejean@users.noreply.github.com>
2024-04-24 15:38:03 -0400

    Update popup.php

--- popup.php
+++ popup.php
@@ -4,19 +4,18 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Get parameters from the URL
-$ScenarioId = $_GET['ScenarioId'];
-$CallerNumber = $_GET['CallerNumber'];
-$CallerName = $_GET['CallerName'];
-$StudentID = $_GET['StudentID'];
-$ContactID = $_GET['ContactID'];
-$ContactName = $_GET['ContactName'];
+$ScenarioId = isset($_GET['ScenarioId']) ? $_GET['ScenarioId'] : '';
+$CallerNumber = isset($_GET['CallerNumber']) ? $_GET['CallerNumber'] : '';
+$CallerName = isset($_GET['CallerName']) ? $_GET['CallerName'] : '';
+$StudentID = isset($_GET['StudentID']) ? $_GET['StudentID'] : '';
+$ContactID = isset($_GET['ContactID']) ? $_GET['ContactID'] : '';
+$ContactName = isset($_GET['ContactName']) ? $_GET['ContactName'] : '';
 
 // Define the URL of your Power Automate flow
-$flowUrl = 'https://prod-74.westus.logic.azure.com:443/workflows/51955eb8bdf84eec89268f4d1b0e9b1f/triggers/manual/paths/invoke?api-version=2016-06-01&ScenarioId={ScenarioId}';
+$flowUrl = 'https://prod-74.westus.logic.azure.com:443/workflows/51955eb8bdf84eec89268f4d1b0e9b1f/triggers/manual/paths/invoke?api-version=2016-06-01&ScenarioId=' . $ScenarioId;
 
 // Define the data to be sent to the flow
 $data = array(
-    'ScenarioId' => $ScenarioId,
     'CallerNumber' => $CallerNumber,
     'CallerName' => $CallerName,
     'StudentID' => $StudentID,
@@ -68,49 +67,49 @@ body {
 </head>
 
 <script language="javascript">
-	function F_Launch(IN){
-		var V_URL="https://collegelacite.lightning.force.com/lightning/r/Contact/"+IN+"/view";
-		window.open(V_URL,'Landis-SF');
-		return;	
-	}
+    function F_Launch(IN){
+        var V_URL="https://collegelacite.lightning.force.com/lightning/r/Contact/"+IN+"/view";
+        window.open(V_URL,'Landis-SF');
+        return; 
+    }
 </script>
 
 <body>
-	<table width="95%" border="4" align="center">
-	  <tbody>
-	    <tr>
-	      <td align="center" valign="middle" nowrap="nowrap" bgcolor="#000000" style="color: #FFFFFF; font-size: 24px; font-weight: bold;">Landis - Salesforce</td>
+    <table width="95%" border="4" align="center">
+      <tbody>
+        <tr>
+          <td align="center" valign="middle" nowrap="nowrap" bgcolor="#000000" style="color: #FFFFFF; font-size: 24px; font-weight: bold;">Landis - Salesforce</td>
         </tr>
       </tbody>
-	</table>
-	<table width="90%" border="2" align="center">
-	  <tbody>
-	    <tr>
-	      <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">ScenarioId</td>
-	      <td align="left" valign="middle" bgcolor="#FFFFFF" style="font-size: 18px; color: #000000;"><?php echo $ScenarioId; ?></td>
+    </table>
+    <table width="90%" border="2" align="center">
+      <tbody>
+        <tr>
+          <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">ScenarioId</td>
+          <td align="left" valign="middle" bgcolor="#FFFFFF" style="font-size: 18px; color: #000000;"><?php echo $ScenarioId; ?></td>
         </tr>
-	    <tr>
-	      <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">CallerNumber</td>
-	      <td align="left" valign="middle" bgcolor="#FFFFFF" style="font-size: 18px; color: #000000;"><?php echo $CallerNumber; ?></td>
+        <tr>
+          <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">CallerNumber</td>
+          <td align="left" valign="middle" bgcolor="#FFFFFF" style="font-size: 18px; color: #000000;"><?php echo $CallerNumber; ?></td>
         </tr>
-	    <tr>
-	      <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">CallerName</td>
-	      <td align="left" valign="middle" bgcolor="#FFFFFF" style="font-size: 18px; color: #000000;"><?php echo $CallerName; ?></td>
+        <tr>
+          <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">CallerName</td>
+          <td align="left" valign="middle" bgcolor="#FFFFFF" style="font-size: 18px; color: #000000;"><?php echo $CallerName; ?></td>
         </tr>
-	    <tr>
-	      <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">StudentID</td>
-	      <td align="left" valign="middle" bgcolor="#FFFFFF" style="font-size: 18px; color: #000000;"><?php echo $StudentID; ?></td>
+        <tr>
+          <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">StudentID</td>
+          <td align="left" valign="middle" bgcolor="#FFFFFF" style="font-size: 18px; color: #000000;"><?php echo $StudentID; ?></td>
         </tr>
       </tbody>
 </table>
-	<p>&nbsp;</p>
-	<table width="80%" border="4" align="center">
-	  <tbody>
-	    <?php for($i = 0; $i < count($ContactID); $i++): ?>
-	    <tr>
-	      <td align="center" valign="middle" bgcolor="#9297FF" onClick="F_Launch('<?php echo trim($ContactID[$i]); ?>')"><?php echo trim($ContactName[$i]); ?><BR><?php echo trim($ContactID[$i]); ?></td>
+    <p>&nbsp;</p>
+    <table width="80%" border="4" align="center">
+      <tbody>
+        <?php for($i = 0; $i < count($ContactID); $i++): ?>
+        <tr>
+          <td align="center" valign="middle" bgcolor="#9297FF" onClick="F_Launch('<?php echo trim($ContactID[$i]); ?>')"><?php echo trim($ContactName[$i]); ?><BR><?php echo trim($ContactID[$i]); ?></td>
         </tr>
-		<?php endfor; ?>
+        <?php endfor; ?>
       </tbody>
 </table>
 </body>
