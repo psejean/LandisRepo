b7a7e59bd0ff288a0b22991e9ff8c0b09b0c832c
psejean <150192056+psejean@users.noreply.github.com>
2024-04-25 00:03:21 -0400

    Update popup.php

--- popup.php
+++ popup.php
@@ -1,20 +1,18 @@
+<?PHP
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Function to log messages to a file
-// function logMessage($message) {
-//     $logFile = 'salesforce_logs.txt';
-//     $timestamp = date('Y-m-d H:i:s');
-//     $logMessage = "[$timestamp] $message\n";
-//     file_put_contents($logFile, $logMessage, FILE_APPEND | LOCK_EX);
-// }
+// Function to log messages to the PHP error log
+function logMessage($message) {
+    error_log($message);
+}
 
 // Get ScenarioId from the URL
 $ScenarioId = $_GET['ScenarioId'];
 
 // Log the start of the script
-// logMessage("Script execution started.");
+logMessage("Script execution started.");
 
 // Define Salesforce credentials
 $salesforceUsername = 'edev_salesforce@collegelacite.ca.devfull';
@@ -50,14 +48,7 @@ LtUk7GtR6zZ4iYknt0KxBAA=
 -----END PRIVATE KEY-----';
 
 // Log Salesforce credentials
-// logMessage("Salesforce credentials: Username - $salesforceUsername, ClientId - $salesforceClientId");
-
-// Include Salesforce JWT token generation library
-require_once('jwt/JWT.php');
-use \Firebase\JWT\JWT;
-
-// Log the inclusion of JWT library
-// logMessage("JWT library included.");
+logMessage("Salesforce credentials: Username - $salesforceUsername, ClientId - $salesforceClientId");
 
 // Generate JWT token
 $issuedAt = time();
@@ -70,18 +61,18 @@ $payload = array(
 );
 
 // Log JWT payload
-// logMessage("JWT payload: " . json_encode($payload));
+logMessage("JWT payload: " . json_encode($payload));
 $jwt = JWT::encode($payload, $salesforcePrivateKey, 'RS256');
 
 // Log JWT token
-// logMessage("JWT token generated: $jwt");
+logMessage("JWT token generated: $jwt");
 
 // Salesforce API endpoint for custom object query
 $salesforceQueryUrl = 'https://collegelacite--devfull.sandbox.lightning.force.com/services/data/v59.0/query/?q=';
 $query = "SELECT CallerNumber__c, CallerName__c, StudentID__c, Contact__c, ContactName__c, Name FROM ContactCallLog__c WHERE Name='$ScenarioId'";
 
 // Log the Salesforce query
-//logMessage("Salesforce query: $query");
+logMessage("Salesforce query: $query");
 
 // Set up cURL session for Salesforce API call
 $ch = curl_init();
@@ -90,7 +81,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer $jwt"));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 // Log the cURL setup
-// logMessage("cURL session set up for Salesforce API call.");
+logMessage("cURL session set up for Salesforce API call.");
 
 // Execute cURL request to Salesforce API
 $response = curl_exec($ch);
@@ -98,11 +89,11 @@ $response = curl_exec($ch);
 // Check for errors
 if(curl_error($ch)) {
     // Log cURL error
-    // logMessage("cURL error: " . curl_error($ch));
+    logMessage("cURL error: " . curl_error($ch));
 }
 
 // Log the cURL response
-// logMessage("cURL response: $response");
+logMessage("cURL response: $response");
 
 // Close cURL session
 curl_close($ch);
@@ -111,7 +102,7 @@ curl_close($ch);
 $result = json_decode($response, true);
 
 // Log Salesforce response
-// logMessage("Salesforce response: " . json_encode($result));
+logMessage("Salesforce response: " . json_encode($result));
 
 // Check if $result is empty or null or if records are empty
 if (empty($result) || !isset($result['records']) || empty($result['records'])) {
@@ -126,5 +117,72 @@ if (empty($result) || !isset($result['records']) || empty($result['records'])) {
     $ContactName = isset($result['records'][0]['ContactName__c']) ? $result['records'][0]['ContactName__c'] : '';
 
     // Log extracted data
-    // logMessage("Extracted data: CallerNumber - $CallerNumber, CallerName - $CallerName, StudentID - $StudentID, ContactID - $ContactID, ContactName - $ContactName");
+    logMessage("Extracted data: CallerNumber - $CallerNumber, CallerName - $CallerName, StudentID - $StudentID, ContactID - $ContactID, ContactName - $ContactName");
 }
+?>
+
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-Equiv="Cache-Control" Content="no-cache">
+<meta http-Equiv="Pragma" Content="no-cache">
+<meta http-Equiv="Expires" Content="0">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Landis-Salesforce-Popup</title>
+<style type="text/css">
+body {
+    background-color: #FFFFFF;
+    color: #000000;
+    font-size: 18px;
+}
+</style>
+</head>
+
+<script language="javascript">
+	function F_Launch(IN){
+		var V_URL="https://collegelacite.lightning.force.com/lightning/r/Contact/"+IN+"/view";
+		window.open(V_URL,'Landis-SF');
+		return;	
+	}
+</script>
+
+<body>
+	<table width="95%" border="4" align="center">
+	  <tbody>
+	    <tr>
+	      <td align="center" valign="middle" nowrap="nowrap" bgcolor="#000000" style="color: #FFFFFF; font-size: 24px; font-weight: bold;">Landis - Salesforce</td>
+        </tr>
+      </tbody>
+	</table>
+	<table width="90%" border="2" align="center">
+	  <tbody>
+	    <tr>
+	      <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">ScenarioId</td>
+	      <td align="left" valign="middle" bgcolor="#FFFFFF" style="font-size: 18px; color: #000000;"><?PHP echo $ScenarioId; ?></td>
+        </tr>
+	    <tr>
+	      <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">CallerNumber</td>
+	      <td align="left" valign="middle" bgcolor="#FFFFFF" style="font-size: 18px; color: #000000;"><?PHP echo $CallerNumber; ?></td>
+        </tr>
+	    <tr>
+	      <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">CallerName</td>
+	      <td align="left" valign="middle" bgcolor="#FFFFFF" style="font-size: 18px; color: #000000;"><?PHP echo $CallerName; ?></td>
+        </tr>
+	    <tr>
+	      <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">StudentID</td>
+	      <td align="left" valign="middle" bgcolor="#FFFFFF" style="font-size: 18px; color: #000000;"><?PHP echo $StudentID; ?></td>
+        </tr>
+      </tbody>
+</table>
+	<p>&nbsp;</p>
+	<table width="80%" border="4" align="center">
+	  <tbody>
+		  <?PHP for($i=0;$i<count($ContactID);$i++): ?>
+	    <tr>
+	      <td align="center" valign="middle" bgcolor="#9297FF" onClick="F_Launch('<?PHP echo trim($ContactID[$i]); ?>')"><?PHP echo trim($ContactName[$i]); ?><BR><?PHP echo trim($ContactID[$i]); ?></td>
+        </tr>
+		  <?PHP endfor; ?>
+      </tbody>
+</table>
+</body>
+</html>
